148c7565a9f4daf86c011f1f16a355e384b5219c
Ardiyansyah R <arsk9797@gmail.com>
2023-09-03 23:42:01 +0800

    menyelesaikan soal 16

--- soal16.js
+++ soal16.js
@@ -6,8 +6,16 @@
 
 function solution(x,y) {
     // tulis jawabanmu disini
+    // 1. buat kondisi jika uang yang ditarik harus dapat dibagi 5, dan saldo mencukupi ditambah dengan biaya bank 0.5
+    if(x % 5 == 0 && y >= x + 0.5){
+        return y - (x + 0.5)
+    } 
+    // 2. jika kondisi tidak memenuhi maka tampilkan nilai saldo
+    else{
+        return y;
+    }
 
 }
 
-console.log(solution(30,20)) // expected output 89.5
+console.log(solution(30,120)) // expected output 89.5
 console.log(solution(42,100)) // expected output 100
