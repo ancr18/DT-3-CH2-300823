f0dcf269bea60ef15e3b16282bde85c3bdf1f027
Ardiyansyah R <arsk9797@gmail.com>
2023-09-03 23:56:39 +0800

    menyelesaikan soal 17

--- soal17.js
+++ soal17.js
@@ -12,6 +12,27 @@
 
 function solution(n) {
     // tulis jawabanmu disini
+
+    // 1. membuat kondisi jika lebih dari sama dengan 91 sampe 100 maka nilai A
+    if(n >= 91 && n <= 100){
+        return 'A'
+    }
+    // 2. membuat kondisi jika lebih dari sama dengan 81 sampe 90 maka nilai B
+    else if(n >= 81 && n <= 90){
+        return 'B'
+    }
+    // 3. membuat kondisi jika lebih dari sama dengan 71 sampe 80 maka nilai C
+    else if(n >= 71 && n <= 80){
+        return 'C'
+    }
+    // 4. membuat kondisi jika lebih dari sama dengan 61 sampe 70 maka nilai D
+    else if(n >= 61 && n <= 70){
+        return 'D'
+    }
+    // 5. membuat kondisi jika kurang dari sama dengan 60 maka nilai E
+    else if(n <= 60){
+        return 'E'
+    }
 }
 
 console.log(solution(95)) // expected output A
