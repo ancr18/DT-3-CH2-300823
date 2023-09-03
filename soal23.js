f2c24a424fbca29404f2fd85f14542966f01f6c2
Ardiyansyah R <arsk9797@gmail.com>
2023-09-03 22:21:27 +0800

    menyelesaikan soal 23

--- soal23.js
+++ soal23.js
@@ -17,3 +17,20 @@ const students = [
     { name: "Evan", score: 55},
     { name: "Rowling", score: 90}
 ]
+
+// 1. buat perulangan untuk membaca seluruh data yang ada di dalam array.
+for (let i = 0; i < students.length; i++){
+
+    // 2. buat percabangan jika score diatas 70 dan tidak lewat dari 100 maka tampilkan pesan LULUS
+    if(students[i].score > 70 && students[i].score <= 100){
+        console.log(`${students[i].name} selamat Ente LULUS. Nilai ente ${students[i].score}`)
+    }
+    // 3. percabangan jika score diatas 50 dan dibawah 70 maka tampilkan pesan remedial
+    else if(students[i].score > 50 && students[i].score < 70){
+        console.log(`${students[i].name} Waduh Ente REMEDIAL. Nilai ente ${students[i].score}`)
+    }
+    // 4. percabangan jika score dibawah 50 maka tampilkan pesan tidak lulus
+    else if(students[i].score < 50){
+        console.log(`${students[i].name} Ente TIDAK LULUS, ngulang lagi yaah. Nilai ente ${students[i].score}`)
+    }
+}
